58f64ca80a74dddd89ad0fafab785583f1c6f975
JostesMarcel <jostes77@googlemail.com>
2022-02-08 16:12:53 +0100

    JS2.1_DOM_Elements

---
+++ assets/js/main.js
@@ -0,0 +1,10 @@
+let navHome = document.getElementById("navHome")
+let active = document.getElementById("active")
+
+console.log(navHome)
+console.log(navChange)
+
+function changeColor(){
+    navHome.classList.toggle("active")
+    console.log(changeColor)
+}
